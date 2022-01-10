0c8c1437f9c95b505b72fd6f08f3dce92fa0728a
yeoboya_211221_03 <qkrcjsthd@gmail.com>
2022-01-10 19:04:05 +0900

    사연 보내기 화면 구현

---
+++ Babyhoney/Babyhoney/Util/Gradient.swift
@@ -0,0 +1,25 @@
+//
+//  Gradient.swift
+//  Babyhoney
+//
+//  Created by yeoboya_211221_03 on 2022/01/10.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton {
+    
+    func setGradient(color1:UIColor,color2:UIColor){
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.colors = [color1.cgColor,color2.cgColor]
+        gradient.locations = [0.0 , 1.0]
+        gradient.startPoint = CGPoint(x: 0.0, y: 1.0)
+        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradient.frame = bounds
+        gradient.cornerRadius = self.frame.height / 2
+        
+        layer.insertSublayer(gradient, at: 0)
+    }
+    
+}
